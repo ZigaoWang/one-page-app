3dd640431534c0b06d3670c21f08e36c5f0a6ac2
Zigao Wang <zigao.wang24@gmail.com>
2025-06-14 22:25:04 +0800

    refractor: add empty ios project

---
+++ One Page/One Page/One_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_PageApp.swift
+//  One Page
+//
+//  Created by Zigao Wang on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct One_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
